20087cd59298e948ac795957d28269a42b90f62d
unknown <asd2017591@gmail.com>
2023-09-30 12:43:37 +0300

    secound commit

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from users.forms import CustomUserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.models import User
 # Create your views here.
 def dashboard(request):
     return render (request, "users/dashboard.html")
@@ -17,6 +18,10 @@ def register(request):
     elif request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect(reverse("dashboard"))
+            if not User.objects.filter(username=form.cleaned_data['username']).exists() and \
+            not User.objects.filter(email=form.cleaned_data['email']).exists():
+                user = form.save()
+                login(request, user)
+                return redirect(reverse("dashboard"))
+        
+        return render(request, "users/register.html",  {"form": CustomUserCreationForm(), "error_message": "Email or Username is not unique"})
